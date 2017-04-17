34ec12596956c2cd84b4508f01fec458b8ef22b3
Alex <anassif2@calstatela.edu>
2017-04-16 17:13:39 -0700

    text detector

--- app/src/main/java/com/example/raymond/barbro/utilities/SearchDrinkView.java
+++ app/src/main/java/com/example/raymond/barbro/utilities/SearchDrinkView.java
@@ -6,6 +6,10 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -20,69 +24,74 @@ public class SearchDrinkView extends View {
     //MaskFilter  mBlur;
     Bitmap mBitmap;
     Canvas  mCanvas;
-    Path mPath;
     Paint mBitmapPaint;
     Bitmap myBitmap;
+    private int height;
+    private int width;
+    private int initialTop;
+    private int initialBottom;
+    private int initialRight;
+    private int intialLeft;
+    private RectF rect;
 
 
     public SearchDrinkView(Context context, Bitmap b) {
         super(context);
-        mPaint = new Paint();
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.TRANSPARENT);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
         mPaint.setAntiAlias(true);
-        mPaint.setDither(true);
-        mPaint.setColor(0xFFFF0000);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeJoin(Paint.Join.ROUND);
-        mPaint.setStrokeCap(Paint.Cap.ROUND);
-        mPaint.setStrokeWidth(20);
 
-        mPath = new Path();
         mBitmapPaint = new Paint();
-        mBitmapPaint.setColor(Color.RED);
+        mBitmapPaint.setARGB(128, 255, 255, 255);
         myBitmap = Bitmap.createBitmap(b);
+        width = myBitmap.getWidth();
+        height = myBitmap.getHeight();
+        initialTop = 10;
+        intialLeft = 10;
+        initialRight = width - 10;
+        initialBottom = initialTop + 100;
+        rect = new RectF(intialLeft, initialTop, initialRight, initialBottom);
     }
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         mBitmap = myBitmap.copy(Bitmap.Config.ARGB_8888, true);
-        mCanvas = new Canvas(mBitmap);
+        //mCanvas = new Canvas(mBitmap);
+
     }
     @Override
     public void draw(Canvas canvas) {
         // TODO Auto-generated method stub
         super.draw(canvas);
-        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
-        canvas.drawPath(mPath, mPaint);
-
+        canvas.drawBitmap(mBitmap, 0, 0, null);
+        canvas.drawRect(rect, mBitmapPaint);
+        //canvas.drawPath(mPath, mPaint);
+        invalidate();
     }
 
     private float mX, mY;
     private static final float TOUCH_TOLERANCE = 4;
 
     private void touch_start(float x, float y) {
-        //mPath.reset();
-        mPath.moveTo(x, y);
+
         mX = x;
         mY = y;
+        Log.d("mx and my", mX + " " + mY);
     }
     private void touch_move(float x, float y) {
-        float dx = Math.abs(x - mX);
-        float dy = Math.abs(y - mY);
-        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-            mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+        float dx = x - mX;
+        float dy = y - mY;
+        //if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            rect.offset(dx, dy);
             mX = x;
             mY = y;
-        }
+            Log.d("mx1 and my1", mX + " " + mY);
+        //}
     }
-    private void touch_up() {
-        mPath.lineTo(mX, mY);
-        // commit the path to our offscreen
-        mCanvas.drawPath(mPath, mPaint);
-        //mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SCREEN));
-        // kill this so we don't double draw
-        mPath.reset();
-        // mPath= new Path();
+    private void touch_up(float x, float y) {
+
     }
 
     @Override
@@ -100,7 +109,7 @@ public class SearchDrinkView extends View {
                 invalidate();
                 break;
             case MotionEvent.ACTION_UP:
-                touch_up();
+                touch_up(x, y);
                 invalidate();
                 break;
         }
