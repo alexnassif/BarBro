abddd1932dd1c35f1b4e5db516bff3501e53a5a3
Alex Nassif <alexnassif@Alexs-MBP.home>
2019-01-16 23:29:09 -0800

    continue migrating to AAC

--- app/src/main/java/com/alexnassif/mobile/barbro/DrinkDetailActivity.java
+++ app/src/main/java/com/alexnassif/mobile/barbro/DrinkDetailActivity.java
@@ -17,6 +17,8 @@ import android.widget.TextView;
 
 import com.alexnassif.mobile.barbro.ViewModel.DrinkDetailViewModel;
 import com.alexnassif.mobile.barbro.ViewModel.FavoritesViewModel;
+import com.alexnassif.mobile.barbro.data.AppDatabase;
+import com.alexnassif.mobile.barbro.data.AppExecutors;
 import com.alexnassif.mobile.barbro.data.Drink;
 import com.alexnassif.mobile.barbro.data.DrinkList;
 import com.bumptech.glide.Glide;
@@ -27,10 +29,15 @@ public class DrinkDetailActivity extends AppCompatActivity {
     private int drink;
     private TextView mDrinkNameView;
     private TextView mDrinkIngredientsView;
+    private TextView mRecipeView;
     private ImageView mImageView;
     private DrinkDetailViewModel drinkDetailViewModel;
     private FavoritesViewModel drinkModel;
     private MenuItem favoriteMenuItem;
+    private boolean faveFlag = false;
+    private AppDatabase mDb;
+    private DrinkList mCurrentDrink;
+    private Drink mDrink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,19 +47,23 @@ public class DrinkDetailActivity extends AppCompatActivity {
         drink = intent.getIntExtra("drink", 0);
         Log.d("drinkdetailint" ,drink + "");
 
-        mDrinkNameView = (TextView) findViewById(R.id.drink_name);
-        mDrinkIngredientsView = (TextView) findViewById(R.id.drink_ingredients);
-        mImageView = (ImageView) findViewById(R.id.drink_pic_activity);
+        mDrinkNameView = (TextView) findViewById(R.id.drink_name_novideo);
+        mDrinkIngredientsView = (TextView) findViewById(R.id.drink_ingredients_novideo);
+        mRecipeView = (TextView) findViewById(R.id.drink_recipe_novideo);
+        mImageView = (ImageView) findViewById(R.id.drink_pic_novideo);
         setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        mDb = AppDatabase.getsInstance(this);
 
         drinkDetailViewModel = ViewModelProviders.of(this).get(DrinkDetailViewModel.class);
         drinkDetailViewModel.setDrink(drink);
         drinkDetailViewModel.getDrink().observe(this, new Observer<Drink>() {
             @Override
             public void onChanged(Drink drink) {
+                mDrink = drink;
                 mDrinkNameView.setText(drink.getStrDrink());
-                mDrinkIngredientsView.setText(drink.getStrInstructions());
+                mRecipeView.setText(drink.getStrInstructions());
+                mDrinkIngredientsView.setText(drink.drinkIngredients());
                 Glide.with(mImageView.getContext()).load(drink.getStrDrinkThumb()).into(mImageView);
             }
         });
@@ -62,8 +73,10 @@ public class DrinkDetailActivity extends AppCompatActivity {
             public void onChanged(DrinkList drinkList) {
 
                 if(drinkList != null){
-
+                    mCurrentDrink = drinkList;
                     favoriteMenuItem.setIcon(R.drawable.ic_fave);
+                    faveFlag = true;
+
                 }
 
             }
@@ -86,6 +99,28 @@ public class DrinkDetailActivity extends AppCompatActivity {
 
         if (id == R.id.fave_item) {
 
+            if(faveFlag){
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        mDb.favoritesDao().deleteFavorite(mCurrentDrink);
+                        favoriteMenuItem.setIcon(R.drawable.ic_non_fave);
+                        faveFlag = false;
+                    }
+                });
+            }else{
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        DrinkList newFave = new DrinkList(DrinkDetailActivity.this.mDrink.getStrDrink(),
+                                DrinkDetailActivity.this.mDrink.getStrDrinkThumb(),
+                                String.valueOf(DrinkDetailActivity.this.mDrink.getIdDrink()));
+                        mDb.favoritesDao().insertFavorite(newFave);
+                        faveFlag = true;
+                    }
+                });
+            }
         }
         return true;
     }
