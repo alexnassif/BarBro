dbaa0c4b03d664a4665da9c66c68ace5faf3396a
Alex Nassif <alexnassif@Alexs-MBP.home>
2019-05-16 22:44:50 -0700

    changed favorites primary key

--- app/src/main/java/com/alexnassif/mobile/barbro/data/DrinkList.java
+++ app/src/main/java/com/alexnassif/mobile/barbro/data/DrinkList.java
@@ -1,5 +1,6 @@
 package com.alexnassif.mobile.barbro.data;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
@@ -7,33 +8,28 @@ import androidx.room.PrimaryKey;
 @Entity(tableName = "favorites")
 public class DrinkList {
 
-    @PrimaryKey(autoGenerate = true)
-    private int id;
+    //@PrimaryKey(autoGenerate = true)
+    //private int id;
     private String strDrink;
     private String strDrinkThumb;
+
+    @PrimaryKey
+    @NonNull
     private String idDrink;
 
-    public DrinkList(int id, String strDrink, String strDrinkThumb, String idDrink){
-        this.id = id;
-        this.strDrink = strDrink;
-        this.strDrinkThumb = strDrinkThumb;
-        this.idDrink = idDrink;
-    }
-
-    @Ignore
     public DrinkList(String strDrink, String strDrinkThumb, String idDrink){
-        this.id = id;
         this.strDrink = strDrink;
         this.strDrinkThumb = strDrinkThumb;
         this.idDrink = idDrink;
     }
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
+    /*public int getId() {
+        return id;
+    }*/
+
+    /*public void setId(int id) {
         this.id = id;
-    }
+    }*/
     public String getStrDrink() {
         return strDrink;
     }
@@ -45,6 +41,11 @@ public class DrinkList {
     public String getIdDrink() {
         return idDrink;
     }
+
+    public void setIdDrink(String idDrink) {
+        this.idDrink = idDrink;
+    }
+
     @Override
     public String toString(){
         return strDrink;
